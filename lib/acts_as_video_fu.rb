16746313852329fdf3d6269f30fe31f5bc56a7b6
Matt Darby <matt@matt-darby.com>
2008-10-06 07:15:12 -0400

    Added exception error and cleaned up structure

--- lib/acts_as_video_fu.rb
+++ lib/acts_as_video_fu.rb
@@ -2,8 +2,8 @@ module Mdarby
   module Acts #:nodoc:
     module Acts_as_video_fu #:nodoc:
 
-      YOUTUBE_RE = /^http:\/\/www.youtube.com\/watch\?v=[a-zA-z0-9]*$/
       VIMEO_RE   = /^http:\/\/vimeo.com\/[0-9]*$/
+      YOUTUBE_RE = /^http:\/\/www.youtube.com\/watch\?v=[a-zA-z0-9]*$/
 
       def self.included(base)
         base.extend ClassMethods
@@ -19,25 +19,21 @@ module Mdarby
       end
 
       module SingletonMethods
-        # Add class methods here
       end
 
       module InstanceMethods
-        # Add instance methods here
           
         def kind?
-          if YOUTUBE_RE.match(video_url)
-            "YouTube"
-          elsif VIMEO_RE.match(video_url)
-            "Vimeo"
-          else
-            false
-          end
+          return "YouTube" if YOUTUBE_RE.match(video_url)
+          return "Vimeo" if VIMEO_RE.match(video_url)
+          return false
         end
         
         private
         
         def validate
+          raise "#{self.class.to_s}#video_url doesn't exist!" unless self.respond_to?(:video_url)
+          
           unless video_url.blank?
             errors.add_to_base("Video URL is not recognized") unless self.kind?
           end
