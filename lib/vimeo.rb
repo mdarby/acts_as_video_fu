ca7b83107a0850aa7e86f8f569a1e2fe2d7ec0bd
Matt Darby <matt@matt-darby.com>
2009-03-06 13:02:03 -0500

    Added Exception class to catch blank responses

--- lib/vimeo.rb
+++ lib/vimeo.rb
@@ -1,3 +1,5 @@
+class VimeoError < StandardError; end
+
 class Vimeo
   include HTTParty
   format :json
@@ -7,6 +9,12 @@ class Vimeo
   def initialize(obj)
     @clip_id  = obj.video_url.split('/').last
     @response = self.class.get('/api/oembed.json', :query => {:url => "http://vimeo.com/#{@clip_id}"})
+    
+    raise blank_error(obj) if @response.blank?
+  end
+  
+  def blank_error(obj)
+    raise VimeoError, "Vimeo returned a blank response for this video: #{obj.video_url}"
   end
   
   def thumbnail_url
